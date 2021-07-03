b291138ca486bcdad09b8ba79340821fc1d4b7e6
forChin <chingiz409@gmail.com>
2021-07-03 12:33:53 +0600

    Add wtf moment with nil type

--- nil.go
+++ nil.go
@@ -4,8 +4,15 @@ import (
 	"fmt"
 )
 
-func nilvar() {
+func nilVar() {
 	// You can declare nil variable.
 	nil := 12
 	fmt.Println("nil =", nil) // nil = 12
+}
+
+func nilType() {
+	// Or declare nil type.
+	type nil string
+	var s nil = "Hello"
+	fmt.Println(s) // "Hello"
 }
